585c9b87112af4cc7f1ffe312e42536801268794
Rhythm-gaur-28 <rhythmgaur2005@gmail.com>
2026-05-24 22:04:49 +0530

    change nodemailer

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,13 +1,20 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = transporter;
