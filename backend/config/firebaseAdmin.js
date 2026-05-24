3ed3b9d05364bc973d1e14d7c97e52377b922e8c
Rhythm-gaur-28 <rhythmgaur2005@gmail.com>
2026-05-24 18:50:41 +0530

    Fix Firebase admin for Render deployment

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,6 +1,11 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebaseServiceAccount.json");
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
