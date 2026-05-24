55c290b5bcd3809ae44d5202057efb668f482094
Rhythm-gaur-28 <rhythmgaur2005@gmail.com>
2026-05-25 03:16:45 +0530

    Clean Gmail OAuth config

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,11 +1,6 @@
 const nodemailer = require("nodemailer");
 const { google } = require("googleapis");
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("GOOGLE_CLIENT_ID exists:", !!process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET exists:", !!process.env.GOOGLE_CLIENT_SECRET);
-console.log("GOOGLE_REFRESH_TOKEN exists:", !!process.env.GOOGLE_REFRESH_TOKEN);
-
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
@@ -17,27 +12,23 @@ oauth2Client.setCredentials({
 });
 
 async function createTransporter() {
-  try {
-    const accessToken = await oauth2Client.getAccessToken();
+  const accessTokenResponse = await oauth2Client.getAccessToken();
 
-    console.log("ACCESS TOKEN:", accessToken?.token ? "GENERATED" : "FAILED");
-
-    return nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-        accessToken: accessToken.token,
-      },
-    });
-
-  } catch (err) {
-    console.error("OAUTH ERROR:", err);
-    throw err;
+  if (!accessTokenResponse || !accessTokenResponse.token) {
+    throw new Error("Failed to generate Gmail access token");
   }
+
+  return nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_USER,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: accessTokenResponse.token,
+    },
+  });
 }
 
 module.exports = createTransporter;
